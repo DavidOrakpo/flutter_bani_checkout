d9fd9a827b1599a7a33d6dcdc6cf73bf2ff4343a
david@bani.africa <davidorakpo65@gmail.com>
2023-08-11 22:32:01 +0100

    bug fixes

--- lib/flutter_bani_checkout.dart
+++ lib/flutter_bani_checkout.dart
@@ -1,6 +1,6 @@
 library flutter_bani_checkout;
 
-export 'src/core/baniObject.dart';
+export 'src/core/bani_object.dart';
 export 'src/core/event_response.dart';
 export 'src/presentation/bani_checkout.dart';
 export 'src/presentation/bani_checkout_modal.dart';
